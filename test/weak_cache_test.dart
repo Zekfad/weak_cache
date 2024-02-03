c2ffcf6b2ff511e861f5b19446d7f41db9150d5b
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2024-02-03 20:33:53 +0300

    test: add more edge cases

--- test/weak_cache_test.dart
+++ test/weak_cache_test.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:test/test.dart';
+import 'package:weak_cache/src/debug.dart';
 import 'package:weak_cache/weak_cache.dart';
 
 
@@ -25,8 +26,10 @@ class _Int {
 
 
 void main() {
+  kDebug = true;
+
   group('Test Weak Cache', () {
-    test('Test that objects are disposable.', () async {
+    test('held objects should be garbage collected and removed from cache', () async {
       final events = <String>[];
 
       final cache = WeakCache<int, Object>();
@@ -35,8 +38,8 @@ void main() {
       Object? someObject = Object();
       Object? someObject2 = Object();
 
-      final _someObjectRef =  WeakReference(someObject);
-      final _someObject2Ref =  WeakReference(someObject2);
+      final _someObjectRef = WeakReference(someObject);
+      final _someObject2Ref = WeakReference(someObject2);
 
       // Add them to cache
       cache[0] = someObject;
@@ -90,11 +93,11 @@ void main() {
       );
     });
 
-    test('Test that Weak Cache is disposable.', () async {
+    test('should not prevent itself from garbage collection', () async {
       final events = <String>[];
 
       WeakCache<int, Object>? cache = WeakCache<int, Object>();
-      final _cacheRef =  WeakReference(cache);
+      final _cacheRef = WeakReference(cache);
 
       // Create some objects
       Object? someObject = Object();
@@ -148,14 +151,14 @@ void main() {
       );
     });
 
-    test('Test that Weak Cache is iterable.', () async {
+    test('should support iteration', () async {
       final events = <String>[];
 
       WeakCache<int, _Int>? cache = WeakCache();
       final _cacheRef = WeakReference(cache);
 
       _Int? object = _Int(0);
-      final _objectRef =  WeakReference(object);
+      final _objectRef = WeakReference(object);
 
       cache[0] = object;
       for (var i = 1; i < 100; i++)
@@ -179,6 +182,8 @@ void main() {
         expect(kv.key == kv.value.i, isTrue);
         j++;
       });
+
+      // Some objects may be removed while testing
       expect(j, allOf(lessThanOrEqualTo(100), greaterThanOrEqualTo(1)));
 
       await Future<void>.delayed(const Duration(seconds: 1));
@@ -211,5 +216,173 @@ void main() {
         ]),
       );
     });
+
+    // In previous version mutex state was frozen for new values,
+    // meaning that when you add new value while iterating, this value
+    // would be places unconditionally to remove queue, even is mutex is no
+    // longer held.
+    test('mutex state should update after iteration is done in finalizer', () async {
+      final events = <String>[];
+
+      WeakCache<int, Object>? cache = WeakCache<int, Object>();
+      final _cacheRef = WeakReference(cache);
+
+      // create objects
+      Object? object1 = Object();
+      final _object1Ref = WeakReference(object1);
+      Object? object2 = Object();
+      final _object2Ref = WeakReference(object2);
+
+      // add first to cache
+      cache[0] = object1;
+
+      // lock mutex
+      // ignore: unused_local_variable
+      Iterable<Object>? values = cache.values;
+
+      // add second object
+      cache[1] = object2;
+
+      final _cacheRefDone = expectAsync0<bool>(() {
+        events.add('cache');
+        return false;
+      });
+
+      final _object1RefDone = expectAsync0<bool>(() {
+        events.add('object 1');
+        return false;
+      });
+
+      final _object2RefDone = expectAsync0<bool>(() {
+        events.add('object 2');
+        return false;
+      });
+
+      expect(identical(cache, _cacheRef.target), isTrue);
+      expect(identical(cache[0], object1), isTrue);
+      expect(identical(cache[0], _object1Ref.target), isTrue);
+      expect(identical(cache[1], object2), isTrue);
+      expect(identical(cache[1], _object2Ref.target), isTrue);
+
+      cache = null;
+      object1 = null;
+
+      // unlock mutex
+      values = null;
+
+      // wait for first object to GC
+      await parallelLoops([
+        () {
+          // ignore: unused_local_variable
+          final bigChunkOfData = '0' * 1024 * 1024 * 50; // about 50mb of data.
+          if (_object1Ref.target != null)
+            return true;
+          return _object1RefDone();
+        },
+      ]);
+
+      // remove 2nd object
+      object2 = null;
+
+      // wait for 2nd object and cache to GC
+      await parallelLoops([
+        () {
+          // ignore: unused_local_variable
+          final bigChunkOfData = '0' * 1024 * 1024 * 50; // about 50mb of data.
+          if (_cacheRef.target != null)
+            return true;
+          return _cacheRefDone();
+        },
+        () {
+          // ignore: unused_local_variable
+          final bigChunkOfData = '0' * 1024 * 1024 * 50; // about 50mb of data.
+          if (_object2Ref.target != null)
+            return true;
+          return _object2RefDone();
+        },
+      ]);
+
+      expect(_cacheRef.target, isNull);
+      expect(_object1Ref.target, isNull);
+      expect(_object2Ref.target, isNull);
+      expect(
+        events,
+        unorderedEquals([
+          'object 1',
+          'object 2',
+          'cache',
+        ]),
+      );
+    });
+
+    test('softly support concurrent modification', () async {
+      final events = <String>[];
+      var modificationDone = false;
+
+      WeakCache<int, _Int>? cache = WeakCache();
+      final _cacheRef = WeakReference(cache);
+
+      _Int? object = _Int(0);
+      final _objectRef = WeakReference(object);
+
+      final _cacheRefDone = expectAsync0<bool>(() {
+        events.add('cache');
+        return false;
+      });
+
+      final _objectRefDone = expectAsync0<bool>(() {
+        events.add('object');
+        return false;
+      });
+
+      final _modificationDone = expectAsync0<void>(max: -1, () {
+        modificationDone = true;
+      });
+
+      cache[0] = object;
+      for (var i = 1; i < 100; i++)
+        cache[i] = _Int(i);
+
+      object = null;
+
+      await parallelLoops([
+        () {
+          for (final MapEntry(:key) in cache!.entries) {
+            if (key > 50) {
+              cache!.remove(key);
+              _modificationDone();
+            }
+          }
+          cache = null;
+          return false;
+        },
+        () {
+          // ignore: unused_local_variable
+          final bigChunkOfData = '0' * 1024 * 1024 * 50; // about 50mb of data.
+          if (_cacheRef.target != null)
+            return true;
+          return _cacheRefDone();
+        },
+        () {
+          // ignore: unused_local_variable
+          final bigChunkOfData = '0' * 1024 * 1024 * 50; // about 50mb of data.
+          if (_objectRef.target != null)
+            return true;
+          return _objectRefDone();
+        },
+      ]);
+
+      expect(_cacheRef.target, isNull);
+      expect(_objectRef.target, isNull);
+      expect(
+        events,
+        unorderedEquals([
+          'object',
+          'cache',
+        ]),
+      );
+      expect(modificationDone, isTrue);
+    });
+
   });
 }
